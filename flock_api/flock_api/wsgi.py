807efc135853771a6ee2a2963647ae5250f0498e
Tyler Kotkin <Tylerkotkin@gmail.com>
2016-02-03 13:18:07 -0500

    django app initialized

---
+++ flock_api/flock_api/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for flock_api project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "flock_api.settings")
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
